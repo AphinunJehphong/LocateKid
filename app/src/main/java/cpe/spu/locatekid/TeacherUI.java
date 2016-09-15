23b05151de27b3e4d9fd5b97e9dc2a18e6fb11e7
golfzayo <aphinun_jeh@hotmail.com>
2016-09-15 16:57:33 +0700

    upload pic success

--- app/src/main/java/cpe/spu/locatekid/TeacherUI.java
+++ app/src/main/java/cpe/spu/locatekid/TeacherUI.java
@@ -1,10 +1,12 @@
 package cpe.spu.locatekid;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +17,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Picasso;
+
 import org.jibble.simpleftp.SimpleFTP;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.File;
 
@@ -27,13 +38,14 @@ public class TeacherUI extends AppCompatActivity implements View.OnClickListener
     private ImageView avataImageView;
     private String[] loginStrings;
     private String imagePathString, imageNameString;
+    private static final String urlPHP = "http://swiftcodingthai.com/golf1/edit_image_teacher.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_ui);
 
-        //Bind Widget
+        //Widget
         nameTextView = (TextView) findViewById(R.id.textView8);
         surnameTextView = (TextView) findViewById(R.id.textView9);
         phoneTextView = (TextView) findViewById(R.id.textView10);
@@ -41,6 +53,12 @@ public class TeacherUI extends AppCompatActivity implements View.OnClickListener
 
         //Get Value From Intent
         loginStrings = getIntent().getStringArrayExtra("Login");
+
+        //เช็ค image ว่ารูปมีไหม
+        if (loginStrings[4].length() != 0) {
+            loadImageAvatar(loginStrings[0]);
+
+        }// if
 
         //Show Text
         nameTextView.setText("ชื่อ : " + loginStrings[1]);
@@ -53,8 +71,64 @@ public class TeacherUI extends AppCompatActivity implements View.OnClickListener
 
     }   // Main Method
 
+    private void loadImageAvatar(String id) {
 
+        Log.d("15SepV3", "Load image at id ==> " + id);
+        LoadImage loadImage = new LoadImage(this, id);
+        loadImage.execute();
 
+    }//Loadimage
+
+    private class LoadImage extends AsyncTask<Void, Void, String>
+    {
+        private Context context;
+        private String idString;
+        private static final String urlPHPimage = "http://swiftcodingthai.com/golf1/get_image_teacher_where_id.php";
+        public LoadImage(Context context, String idString) {
+            this.context = context;
+            this.idString = idString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("ID_Teacher", idString)
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlPHPimage).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("15SepV3", "e doIn ==>" + e.toString());
+                return null;
+            }
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("15SepV3", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                JSONObject jsonObject = jsonArray.getJSONObject(0); //มาจากค่าที่รับมาคือ loginstring0
+                String strURLimage = jsonObject.getString("Pic_Teacher"); //ดึงค่าที่ต้องการมาโชว์
+                Picasso.with(context).load(strURLimage).resize(120, 150).into(avataImageView);//ไม่ว่ารูปจะขนาดเท่าไหร่จัดให้เป็นขนาดนี้เลย
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }//onPost
+    }// method การโหลดรูปภาพ
 
     @Override
     public void onClick(View view) {
@@ -65,7 +139,7 @@ public class TeacherUI extends AppCompatActivity implements View.OnClickListener
                 confirmEditImage();
                 break;
 
-        }   // switch
+        }   // switch เมื่อคลิกที่รูปจะเกิดกิจกรรมนี้ขึ้น
 
     }   // onClick
 
@@ -148,6 +222,8 @@ public class TeacherUI extends AppCompatActivity implements View.OnClickListener
             simpleFTP.stor(new File(imagePathString));
             simpleFTP.disconnect();
 
+            updateImageOnMySQL();
+
             Toast.makeText(this, "Upload " + imagePathString + "finish",
                     Toast.LENGTH_SHORT).show();
 
@@ -156,6 +232,57 @@ public class TeacherUI extends AppCompatActivity implements View.OnClickListener
         }
 
     }// method อัพรูปขึ้น server
+
+    private void updateImageOnMySQL() {
+
+        EditPicTeacher editPicTeacher = new EditPicTeacher(this);
+        editPicTeacher.execute();
+
+    } // uploadImageOnMySQL
+
+    private class EditPicTeacher extends AsyncTask<Void, Void, String> {
+
+        //ประกาศตัวแปร
+        private Context context;
+
+        public EditPicTeacher(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("ID_Teacher", loginStrings[0])
+                        .add("Pic_Teacher", "http://swiftcodingthai.com/golf1/Image/" + imageNameString)
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlPHP).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("15SepV2", "e doIn ==> " + e.toString());
+                return null;
+            }
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("15SepV2", "Result ==> " + s);
+            loadImageAvatar(loginStrings[0]);
+
+        }//onPost
+
+    }//editPicTeacher
+
 
     private String myFindPathOfImage(Uri uri) {
 
